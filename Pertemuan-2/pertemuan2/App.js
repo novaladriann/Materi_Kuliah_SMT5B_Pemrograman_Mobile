80590d7dd436edcc975d345b52a2797534abd715
novaladriann <novalstudy96@gmail.com>
2026-09-14 12:14:32 +0700

    Pertemuan-2/pertemuan2/App.js

--- Pertemuan-2/pertemuan2/App.js
+++ Pertemuan-2/pertemuan2/App.js
@@ -4,7 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Nama : Noval Adrian</Text>
+      <Text>NIM : 2488010018</Text>
+      <Text>Asal sekolah : SMAN 8 Kota Cirebon</Text>
+      <Text>Cita-cita: AI Engineer</Text>
+      <Text>Rencana menggapai cita-cita: Belajar dan berlatih secara konsisten</Text>
       <StatusBar style="auto" />
     </View>
   );
